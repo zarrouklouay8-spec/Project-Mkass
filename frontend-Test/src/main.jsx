de5e1938aa805e82074fc826832e57f89c8c6072
zarrouklouay8-spec <zarrouklouay8@gmail.com>
2026-09-19 15:35:10 +0100

    Update main.jsx

--- frontend-Test/src/main.jsx
+++ frontend-Test/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import './styles/global.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 requestAnimationFrame(() => {

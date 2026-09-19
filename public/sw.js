dbfa3b678f4489f3efd8badd98b18eeacfb612f1
zarrouklouay8-spec <zarrouklouay8@gmail.com>
2026-09-19 15:20:26 +0100

    Delete public directory

--- public/sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE_NAME = 'mkass-consistent-logo-v3';
-self.addEventListener('install', event => self.skipWaiting());
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
